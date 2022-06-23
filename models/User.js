8010d8dabf3843541f0d512507ca5fc67eb8d939
Jose Ueslei De Moraes Ferreira <60453278+uesleiMF@users.noreply.github.com>
2022-06-22 21:27:43 -0300

    oi

--- models/User.js
+++ models/User.js
@@ -1,26 +1,41 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+    img: {
+      type: String,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
 
-const UserSchema = new mongoose.Schema({
-	name: {
-		type: String,
-		required: true,
-	},
-	email: {
-		type: String,
-		required: true,
-		unique: true,
-	},
-	password: {
-		type: String,
-		required: true,
-	},
-	avatar: {
-		type: String,
-	},
-	date: {
-		type: Date,
-		default: Date.now,
-	},
-});
-
-module.exports = User = mongoose.model("user", UserSchema);
+export default mongoose.model("User", UserSchema);
