85a5dc0716aceb2b367cde83a6b7a62e5043add5
Jose Ueslei De Moraes Ferreira <60453278+uesleiMF@users.noreply.github.com>
2022-06-24 19:44:16 -0300

    sim

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require ('express');
+const cookieParser = require ('cookie-parser');
+const cors = require ('cors');
+const path = require ('path');
+const mongoose = require ('mongoose');
+const routes= require ('./src/routes');
+
+
+
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+mongoose.connect('mongodb://localhost:27017/login',{
+    useUnifiedTopology:true,
+    useNewUrlParser:true,
+    //useFindAndModify:false
+},function(err){
+   if(err){
+    console.log(err)
+   }else{
+    console.log('MongoDB CONECTADO com  sucesso')
+   }
+
+
+})
+
+
+
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+
+app.get('/', function(req,res){
+   res.json({message:'Hello World'});
+   });
+   app.listen(port,function(){
+    console.log(`Server runing on port ${port}`)
+
+   }) 
+
+
+ 
